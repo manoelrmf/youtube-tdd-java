ec038e4127c4de9dd8c3845ae19408090c8bcf09
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2022-08-22 12:14:50 -0300

    feat(): cria entidade course

---
+++ src/main/java/br/com/tddjava/tddjava/modules/course/entities/Course.java
@@ -0,0 +1,27 @@
+package br.com.tddjava.tddjava.modules.course.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "courses")
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String description;
+
+    private String name;
+
+    private int workload;
+}
